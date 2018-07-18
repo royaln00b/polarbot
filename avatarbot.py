dca8de89dfa8f1962fd13555063d116b6291c1f0
royaln00b <37339403+royaln00b@users.noreply.github.com>
2018-07-18 21:44:58 +0100

    Update avatarbot.py

--- avatarbot.py
+++ avatarbot.py
@@ -91,10 +91,10 @@ async def channel(ctx,action,*,channel):
 
 #----------------------------------------------------------------------------------------------------------
 #                                              ADMIN COMMANDS
-adminlist = ["Manager"]
+
 @bot.command(pass_context=True)
 async def purge(ctx,num: int):
-	if [y.lower() for y in adminlist] in [y.name.lower() for y in ctx.message.author.roles]:
+	if "Manager" in [y.name.lower() for y in ctx.message.author.roles]:
 		await bot.purge_from(ctx.message.channel,limit=num)
 	else:
 		await bot.say("No")
